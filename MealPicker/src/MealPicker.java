20bf7857bd7ebdbe416e66eac3ec9c1004a4aeff
dlobato85 <dlobato@my.apsu.edu>
2022-05-13 16:54:45 -0500

    Add files via upload

---
+++ MealPicker/src/MealPicker.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class MealPicker {
+
+	
+
+	public String foodPicker(int  x) {
+		String choice = null;
+		Random rand = new Random();
+		
+		
+		
+		//int  mealArray[] = new int[x];
+		
+		return choice;
+	}
+	
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing phase");
+		
+		Scanner sc = new Scanner (System.in);
+		
+			
+		System.out.println("How many food options are you considering?");
+		int amount = sc.nextInt();
+		
+		
+		
+		
+		//System.out.println(amount);
+		
+	}
+
+	
+	
+	
+	
+}
